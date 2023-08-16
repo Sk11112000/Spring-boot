74db2bc31ae3534d9773f33383ebf6b18a1535ea
Sk11112000 <sunnykulshrestha1111@gmail.com>
2023-08-16 22:30:32 +0530

    Move to backend folder

---
+++ src/main/java/com/sunny/customer/CustomerController.java
@@ -0,0 +1,41 @@
+package com.sunny.customer;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("customers")
+    public List<Customer> getCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("{customerId}")
+    public Customer getCustomer(@PathVariable Integer customerId) {
+        return customerService.getCustomer(customerId);
+    }
+    @PostMapping()
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest request)
+    {
+        customerService.addCustomer(request);
+    }
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable Integer customerId)
+    {
+
+        customerService.deleteCustomerById(customerId);
+    }
+    @PutMapping("update/{customerId}")
+    public void updateCustomerById(@PathVariable Integer customerId,@RequestBody CustomerUpdateRequest updateRequest)
+    {
+        customerService.updateCustomer(customerId,updateRequest);
+    }
+}
---
+++ src/main/java/com/sunny/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,4 @@
+package com.sunny.customer;
+
+public record CustomerRegistrationRequest(String name,String email,Integer age) {
+}
