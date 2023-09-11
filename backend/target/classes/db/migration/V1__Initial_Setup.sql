27be1e54dc51a71b229a07325ace30dd9b70ae3a
Sk11112000 <92803826+Sk11112000@users.noreply.github.com>
2023-09-11 14:51:10 +0530

    Delete backend/target directory

--- backend/target/classes/db/migration/V1__Initial_Setup.sql
+++
@@ -1,8 +0,0 @@
-create table customer(
-    id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL ,
-    age INT NOT NULL,
-    gender TEXT NOT NULL,
-    password TEXT NOT NULL
-)
